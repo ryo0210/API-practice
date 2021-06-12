3b0aae3c585a7454d7862ceceb5115d3971fc87b
ryo0210 <yone0217@icloud.com>
2021-06-13 03:26:29 +0900

    [Add]:Deleteメソッドを追加した。

--- app/app.js
+++ app/app.js
@@ -72,74 +72,103 @@ app.get("/api/v1/search", function(req, res) {
 
 
 // DBクエリ実行用の関数
-const que = async (sql, connection, res) => {
+const que = async (sql, connection) => {
     return new Promise((resolve, reject) => {
         connection.query(sql, (err) => {
             if (err) {
-                res.status(500).send(err)
                 return reject()
             } else {
-                res.json({message: "新規ユーザーの登録に成功しました。"})
                 return resolve()
             }
         })
     })
 }
 
-
-
-
 // Create a new user
 app.post("/api/v1/users", async function(req, res) {
-    connection.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected POST method!!!");
-    });
+    if (!req.body.name || req.body.name === "") {
+        res.status(400).send({"error": "ユーザー名が指定されていません。"})
+    } else {
+        connection.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected POST method!!!");
+        });
 
-    const user_id = req.body.user_id
-    const user_name = req.body.user_name ? req.body.user_name : ""
-    const user_icon = req.body.user_icon ? req.body.user_icon : ""
-    
-    await que(
-        `INSERT INTO users (name) VALUES ("${user_name}")`,
-        connection,
-        res
-    )
+        const user_id = req.body.user_id
+        const user_name = req.body.name ? req.body.name : ""
+        const user_icon = req.body.user_icon ? req.body.user_icon : ""
+        
+        try {
+            await que(
+                `INSERT INTO users (name) VALUES ("${user_name}")`,
+                connection
+            )
+            res.status(201).send({"message": "新規ユーザーの追加に成功しました。"})
+        } catch (e) {
+            res.status(500).send({"error": e})
+        }
+    }
 })
 
 
 
 // Update user data
 app.put("/api/v1/users/:user_id", async function(req, res) {
+    if (!req.body.name || req.body.name === "") {
+        res.status(400).send({"error": "ユーザー名が指定されていません。"})
+    } else {
+        connection.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected POST method!!!");
+        });
+        const user_id = req.params.user_id
+
+        // 現在のユーザーを取得する
+        connection.query(`SELECT * FROM users WHERE id = ${user_id}`, async function(err, row) {
+            if (!row) {
+                res.status(204).send({"error": "指定したユーザーが見つかりません。"})
+            } else {
+                const name = req.body.name ? req.body.name : row.name
+                const user_icon = req.body.user_icon ? req.body.user_icon : row.user_icon
+
+                try {
+                    await que(
+                        `UPDATE users SET name="${name}" WHERE id=${user_id}`,
+                        connection,
+                    )
+                    res.status(200).send({message: "ユーザー情報を更新しました。"})
+                } catch (e) {
+                    res.status(500).send({error: e})
+                }
+            }
+        });
+    };
+});
+
+// Delete user data
+app.delete("/api/v1/users/:user_id", async function(req, res) {
     connection.connect(function(err) {
         if (err) throw err;
-        console.log("Connected PUT method!!!");
+        console.log("Connected DELETE method!!!");
     });
     const user_id = req.params.user_id
 
-    // 現在のユーザーを取得する
+    // ユーザーが存在しているか確認
     connection.query(`SELECT * FROM users WHERE id = ${user_id}`, async function(err, row) {
-        const name = req.body.name ? req.body.name : row.name
-        const user_icon = req.body.user_icon ? req.body.user_icon : row.user_icon
-
-
-        try {
-            await que(
-              `UPDATE users SET name="${name}" WHERE id=${user_id}`,
-              connection,
-              res.json({message: "ユーザーの更新に成功しました。"})
-            )
-            res.status(200).send({message: "ユーザー情報を更新しました。"})
-        } catch (e) {
-            res.status(500).send({error: e})
+        if (!row) {
+            res.status(204).send({"error": "指定したユーザーが見つかりません。"})
+        } else {
+            try {
+                await que(
+                    `DELETE FROM users WHERE id=${user_id}`,
+                    connection,
+                )
+                res.status(200).send({message: "ユーザーを削除しました。"})
+            } catch (e) {
+                res.status(500).send({error: e})
+            }
         }
-
-        // await que(
-        //     `UPDATE users SET name="${name}" WHERE id=${user_id}`,
-        //     connection,
-        //     res.json({message: "ユーザーの更新に成功しました。"})
-        // )
-    });
+    })
 });
 
 
